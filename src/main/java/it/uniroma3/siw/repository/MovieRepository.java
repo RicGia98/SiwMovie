65f2a447952929227f23436009935b21c3af3c19
Riccardo Giacinti <rickygia@libero.it>
2023-07-02 17:27:47 +0200

    Commit finale

---
+++ src/main/java/it/uniroma3/siw/repository/MovieRepository.java
@@ -0,0 +1,12 @@
+package it.uniroma3.siw.repository;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.model.Movie;
+
+public interface MovieRepository extends CrudRepository<Movie, Long> {
+	
+    public List<Movie> findAllByYear(Integer year);
+    public boolean existsByTitleAndYear(String title, Integer year);
+}
